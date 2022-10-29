11c3818ffd75e323f806de0e069c0412fbdc405d
alu <dev.bhorta@gmail.com>
2022-10-29 16:31:32 +0600

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+@app.route('/')
+def hello():
+    count = cache.incr('hits')
+    return 'Hello Docker World! I have been seen {} times.\n'.format(count)
